d4561d6a1f3d57db4a9b2a2ddc1b9a0fe1a6c976
Don Najd <dnajd7@gmail.com>
2014-05-11 22:45:30 -0700

    startup script

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -16,10 +16,20 @@ from fluentnao.nao import Nao
 ######################### 
 
 # Broker (must come first)
-naoIp = "nao.local"
+#naoIp = "nao.local"
+naoIp = "192.168.2.15"
 broker.Broker('bootstrapBroker', naoIp=naoIp, naoPort=9559)
 
 # FluentNao
 env = naoenv.make_environment(None)
 log = lambda msg: print(msg) 				# lambda for loggin to the console
 nao = Nao(env, log)
+
+# init
+nao.say('I think')
+nao.wait(.5)
+nao.say('therefore')
+nao.wait(.5)
+nao.say('I')
+nao.wait(.1)
+nao.say('am')
